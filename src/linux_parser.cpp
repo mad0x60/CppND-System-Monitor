afd185fe262e375caca9ba70eb8150ed59699154
Mohamed.Moustafa <mohamed.moustafa@nxp.com>
2022-03-15 21:12:19 +0100

    addressed linux parser todos

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -2,9 +2,12 @@
 #include <unistd.h>
 #include <sstream>
 #include <string>
+#include <cstring>
 #include <vector>
 
 #include "linux_parser.h"
+
+#include "utils.h"
 
 using std::stof;
 using std::string;
@@ -68,49 +71,112 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() {
+  long memTotal =  std::stol(Utils::getTokenInLine(kProcDirectory + kMeminfoFilename, 1, 2));
+  long memFree =  std::stol(Utils::getTokenInLine(kProcDirectory + kMeminfoFilename, 2, 2));
+  return (float)(memTotal - memFree) / memTotal;
+}
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  return std::stol(Utils::getTokenInLine(kProcDirectory + kUptimeFilename, 1, 1));  
+}
 
 // TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
+long LinuxParser::Jiffies() {
+  long totalJiffies = 0;
+  string cpuStatStr = Utils::getLine(kProcDirectory + kStatFilename, 1);
+  std::istringstream cpuStat(cpuStatStr);
+  cpuStat.ignore(5, ' '); // ignoring the cpu token
+  for (string jiffies; cpuStat >> jiffies; totalJiffies += stol(jiffies));
+  return totalJiffies;
+}
+
+// TODO: Read and return the number of active jiffies for the system
+long LinuxParser::ActiveJiffies() {
+  return Jiffies() - IdleJiffies();
+}
+
+// TODO: Read and return the number of idle jiffies for the system
+long LinuxParser::IdleJiffies() { 
+  long idleTime = std::stol(Utils::getTokenInLine(kProcDirectory + kStatFilename, 1, 5));  
+  long ioWaitTime = std::stol(Utils::getTokenInLine(kProcDirectory + kStatFilename, 1, 6)); 
+  return idleTime + ioWaitTime;
+}
+
+// TODO: Read and return CPU utilization
+vector<string> LinuxParser::CpuUtilization() {
+  return { std::to_string((float)(ActiveJiffies())/Jiffies()) };
+}
+
+// TODO: Read and return the total number of processes
+int LinuxParser::TotalProcesses() { 
+  return std::stol(Utils::getTokenInLine(kProcDirectory + kStatFilename, 9, 2));  
+}
+
+// TODO: Read and return the number of running processes
+int LinuxParser::RunningProcesses() { 
+  return std::stol(Utils::getTokenInLine(kProcDirectory + kStatFilename, 10, 2));   
+}
 
 // TODO: Read and return the number of active jiffies for a PID
 // REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::ActiveJiffies(int pid[[maybe_unused]]) { return 0; }
-
-// TODO: Read and return the number of active jiffies for the system
-long LinuxParser::ActiveJiffies() { return 0; }
-
-// TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
-
-// TODO: Read and return CPU utilization
-vector<string> LinuxParser::CpuUtilization() { return {}; }
-
-// TODO: Read and return the total number of processes
-int LinuxParser::TotalProcesses() { return 0; }
-
-// TODO: Read and return the number of running processes
-int LinuxParser::RunningProcesses() { return 0; }
+long LinuxParser::ActiveJiffies(int pid) {
+  string procStatFile = kProcDirectory + std::to_string(pid) + "/" + kStatFilename;
+  return std::stol(Utils::getTokenInLine(procStatFile, 1, 14));
+}
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Command(int pid) { 
+  string procCmdFile = kProcDirectory + std::to_string(pid) + "/" + kCpuinfoFilename;
+  return Utils::getLine(procCmdFile, 1);
+}
 
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Ram(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Ram(int pid) {
+  string procStatusFile = kProcDirectory + std::to_string(pid) + "/" + kStatusFilename;
+
+  // divide by 1000 to convert from KB to MB
+  long vmSizeMB =  std::stol(Utils::getTokenInLine(procStatusFile, 18, 2)) / 1000;
+  return std::to_string(vmSizeMB);
+}
 
 // TODO: Read and return the user ID associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Uid(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Uid(int pid) { 
+    std::string procStatusFile = kProcDirectory + std::to_string(pid) + "/" + kStatusFilename;
+    return Utils::getTokenInLine(procStatusFile, 9, 2);
+}
 
 // TODO: Read and return the user associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::User(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::User(int pid) { 
+  string uid = Uid(pid); // string of UID
+
+  string line;
+  std::vector<string> tokens;
+  std::ifstream filestream (kPasswordPath); // /etc/passwd file
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+
+      // tokenizing
+      for(auto token = strtok(&line[0], ":"); token != NULL; token = strtok(NULL, ":")) {
+        if (uid.compare(token) == 0) {
+          return tokens[0];
+        } else {
+          tokens.push_back(token);
+        }
+      }
+    }
+  }
+  return string();
+}
 
 // TODO: Read and return the uptime of a process
 // REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::UpTime(int pid[[maybe_unused]]) { return 0; }
+long LinuxParser::UpTime(int pid) {
+  std::string procStatFile = kProcDirectory + std::to_string(pid) + "/" + kStatFilename;
+  return std::stol(Utils::getTokenInLine(procStatFile, 1, 14));
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include "ncurses_display.h"
 #include "system.h"
 
+#include <iostream>
+
+// TODO: remove me
+#include "linux_parser.h" 
+
 int main() {
   System system;
-  NCursesDisplay::Display(system);
+
+  int pid = 1;
+
+  std::cout << "OperatingSystem: " << LinuxParser::OperatingSystem() << std::endl;
+  std::cout << "Kernel: " << LinuxParser::Kernel() << std::endl;
+
+  for (auto pid : LinuxParser::Pids())
+  {
+    std::cout << "" << pid << ", ";  
+  }
+  std::cout << std::endl;
+
+  std::cout << "MemoryUtilization: " << LinuxParser::MemoryUtilization() << std::endl;
+  std::cout << "UpTime: " << LinuxParser::UpTime() << std::endl;
+  std::cout << "Jiffies: " << LinuxParser::Jiffies() << std::endl;
+  std::cout << "ActiveJiffies: " << LinuxParser::ActiveJiffies() << std::endl;
+  std::cout << "IdleJiffies: " << LinuxParser::IdleJiffies() << std::endl;
+  std::cout << "CpuUtilization: " << LinuxParser::CpuUtilization()[0] << std::endl;
+  std::cout << "TotalProcesses: " << LinuxParser::TotalProcesses() << std::endl;
+  std::cout << "RunningProcesses: " << LinuxParser::RunningProcesses() << std::endl;
+  std::cout << "ActiveJiffies: " << LinuxParser::ActiveJiffies(pid) << std::endl;
+  std::cout << "Command: " << LinuxParser::Command(pid) << std::endl;
+  std::cout << "Ram: " << LinuxParser::Ram(pid) << std::endl;
+  std::cout << "Uid: " << LinuxParser::Uid(pid) << std::endl;
+  std::cout << "User: " << LinuxParser::User(pid) << std::endl;
+  std::cout << "UpTime: " << LinuxParser::UpTime(pid) << std::endl;
+  // NCursesDisplay::Display(system);
 }
