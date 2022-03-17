d0f66ba5a692ea1a63b58154307b153f28f87126
Mohamed.Moustafa <mohamed.moustafa@nxp.com>
2022-03-17 11:34:33 +0100

    addressed todos in the processor class

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 // TODO: remove me
 #include "linux_parser.h"
 #include "process.h"
+#include "processor.h"
 
 int main() {
   System system;
@@ -13,13 +14,17 @@ int main() {
   int pid = 200089;
 
   Process process(pid);
+  Processor processor;
 
   std::cout << "pid: " << process.Pid() << std::endl;
   std::cout << "CpuUtilization: " << process.CpuUtilization() << std::endl;
   std::cout << "Command: " << process.Command() << std::endl;
   std::cout << "Ram: " << process.Ram() << std::endl;
   std::cout << "User: " << process.User() << std::endl;
-  std::cout << "UpTime: " << process.UpTime() << std::endl;
+  std::cout << "UpTime process: " << process.UpTime() << std::endl;
+  std::cout << "UpTime: " << LinuxParser::UpTime() << std::endl;
+
+  std::cout << "Processor::Utilization: " << processor.Utilization() << std::endl;
 
   // NCursesDisplay::Display(system);
 }
